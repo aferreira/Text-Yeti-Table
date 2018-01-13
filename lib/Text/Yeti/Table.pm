650985e1d7377474da16c3f4a2ff6ebe48b329f8
Adriano Ferreira <a.r.ferreira@gmail.com>
2018-01-12 16:26:27 -0800

    Add a compile-spec step

--- lib/Text/Yeti/Table.pm
+++ lib/Text/Yeti/Table.pm
@@ -12,22 +12,44 @@ our @EXPORT_OK = qw(render_table);
 # default stringification
 my $TO_S = sub { defined $_[0] ? "$_[0]" : "<none>" };
 
+sub _compile_table_spec {
+    my $spec = shift;
+
+    # 'key'
+    # [ 'key', $to_s, 'head' ]
+
+    # { K => 'key', H => 'head', S => $to_s }
+
+    my @columns;
+    for (@$spec) {
+        my @spec = ref $_ ? @$_ : ($_);
+
+        my %spec;
+        $spec{K} = $spec[0];
+        $spec{H} = $spec[2]
+          // do { local $_ = $spec[0]; s/([a-z])([A-Z])/$1 $2/g; uc };
+        $spec{S} = $spec[1] // $TO_S;
+
+        push @columns, \%spec;
+    }
+    return \@columns;
+}
+
 sub _render_table {
     my ( $items, $spec, $io ) = ( shift, shift, shift );
 
+    my @spec = @{ _compile_table_spec($spec) };
+
     my ( @rows, @len );
-    my @spec = map { ref $_ ? $_ : [$_] } @$spec;
-    my @c = map { $_->[0] } @spec;
 
     # Compute table headers
-    my @h = map {
-        $_->[2] // do { local $_ = $_->[0]; s/([a-z])([A-Z])/$1 $2/g; uc }
-    } @spec;
+    my @h = map { $_->{H} } @spec;
     @len = map { length $_ } @h;
     push @rows, \@h;
 
     # Compute table rows, keep track of max length
-    my @to_s = map { $_->[1] // $TO_S } @spec;
+    my @c    = map { $_->{K} } @spec;
+    my @to_s = map { $_->{S} } @spec;
     for my $item (@$items) {
         my @v = map { $to_s[$_]->( $item->{ $c[$_] }, $item ) } 0 .. $#c;
         $len[$_] = max( $len[$_], length $v[$_] ) for 0 .. $#c;
